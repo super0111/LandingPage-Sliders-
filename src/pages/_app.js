0e9965d1079f710dcd8e31d16a5014b39ae0e9b0
SVEN <92024717+super0111@users.noreply.github.com>
2023-04-28 18:24:17 +0200

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import { ThemeProvider } from '@mui/material'
 import Layout from '../components/Layout'
 import theme from '@/themes'
 import '@/styles/globals.css'
-import '../styles/home.css'
+// import '../styles/home.css'
 
 export default function App({ Component, pageProps }) {
   return (
